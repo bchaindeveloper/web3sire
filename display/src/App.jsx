19f13c0db4a2cd89ff4cccd0ee0f81245242818a
bchaindeveloper <bcdev3@protonmail.com>
2022-12-19 15:20:12 -0500

    Navbar

--- display/src/App.jsx
+++ display/src/App.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Route, Routes } from 'react-router-dom';
 import { CampaignDetails, CreateCampaign, Home, Profile } from './pages';
-import { Sidebar, Navbar } from './components'
+import { Sidebar, Navbar, CustomButton } from './components'
 
 const App = () => {
   return (
